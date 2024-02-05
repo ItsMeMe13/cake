d28f9367f3a17f6cd5bdc0b65cd47c75cc5afac6
ItsMeMe131 <26613678-ItsMeMe131@users.noreply.replit.com>
2024-02-05 17:57:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){ */
+//let optionOneImg = document.querySelector("option-one");
+//let buttonOne = document.querySelector(".option-one");
+
+//buttonOne.onclick = function() {
+//opening.style.display = "none";
+// buttons.style.display = "none";
+// optionOne.style.display = "block";
+
+//}; 
+let opening = document.querySelector(".story-opening");
+let buttons = document.querySelector(".buttons");
+let optionOne = document.querySelector(".option-one-screen");
+let buttonOne = document.querySelector(".option-one");
+
+
+buttonOne.onclick = function() {
+  opening.style.display = "none";
+  buttons.style.display = "none";
+  optionOne.style.display = "block";
+}; 
+let optionTwo = document.querySelector(".option-two-screen");
+let buttonTwo = document.querySelector(".option-two");
+
+buttonTwo.onclick = function() {
+  opening.style.display = "none";
+  buttons.style.display = "none";
+  optionTwo.style.display = "block";
+}; 
+let optionThree = document.querySelector(".option-three-screen");
+let buttonThree = document.querySelector(".option-three");
+
+buttonThree.onclick = function() {
+  opening.style.display = "none";
+  buttons.style.display = "none";
+  optionThree.style.display = "block";
+}; 
+let optionFour = document.querySelector(".option-four-screen");
+let buttonFour = document.querySelector(".option-four");
+
+buttonFour.onclick = function() {
+  opening.style.display = "none";
+  buttons.style.display = "none";
+  optionFour.style.display = "block";
+}; 
+let optionFive = document.querySelector(".option-five-screen");
+let buttonFive = document.querySelector(".option-five");
+
+buttonFive.onclick = function() {
+  opening.style.display = "none";
+  buttons.style.display = "none";
+  optionFive.style.display = "block";
+  
+};
+
+
+
+//INSERT_VARIABLE.onclick=function(){
+
+//};
+
+
